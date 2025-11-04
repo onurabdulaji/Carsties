624c63e63a5b780b42d1ebce566f797c7a00a1a1
oabdulaji <oabdulaji@tessa.group>
2025-11-04 12:08:43 +0100

    Changes Section 4.1

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e3bb5fc80a05f915f58c111121f1a0a8aa9c648")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e71beb0c2de0198011a57a28dca578dc9bb6c674")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
